2c2557af8470a550b63b5557f2e2b982a3ff2f3f
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-02-26 15:43:46 -0300

    rename state file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "metabase-solutions-state-tf"
-    key     = "development/infrastrucure.tfstate"
+    key     = "development/infra_dev.tfstate"
     encrypt = "true"
     region  = "us-east-2"
   }
